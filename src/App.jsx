30a46416e7279b6705ceed64ada7f47ee36160f0
Tommi Pöntinen <98650216+tommipontinen76@users.noreply.github.com>
2026-03-13 14:43:58 +0200

    Added header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css'
 import Lemon from './components/Lemon'
 import Balance from './components/Balance'
 import Booster from './components/Booster'
+import Header from './components/Header'
+
 
 function App() {
 
     return (
         <>
             <div>
+                <Header>lemon clicker</Header>
                 <Balance total="157" />
                 <Lemon />
                 <Booster value="3.2" />
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header(props) {
+
+    return (
+        <div className="header">
+            <h1>{props.children}</h1>
+        </div>
+    )
+
+}
+
+export default Header
