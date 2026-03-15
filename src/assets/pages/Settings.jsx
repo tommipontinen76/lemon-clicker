fa884296027003c040bc1f2881300c0a3ce82753
Tommi Pöntinen <98650216+tommipontinen76@users.noreply.github.com>
2026-03-15 03:11:53 +0200

    Added Store and Settings components

---
+++ src/assets/pages/Settings.jsx
@@ -0,0 +1,20 @@
+import Header from '../components/Header'
+
+function Settings(props) {
+    return (
+        <div className="container">
+            <Header balance={props.stats.balance}>settings</Header>
+            <div className="scrollbox">
+                <div className="settings">
+                    <h2>lemon stats</h2>
+                    <div>
+                        TODO stats
+                    </div>
+                </div>
+                TODO reset
+            </div>
+        </div>
+    )
+}
+
+export default Settings
---
+++ src/assets/pages/Store.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header'
+
+function Store(props) {
+    return (
+        <div className="container">
+            <Header balance={props.stats.balance}>store</Header>
+            <div className="scrollbox items">
+                TODO items
+            </div>
+        </div>
+    )
+}
+
+export default Store
