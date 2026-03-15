5db20d8e3fec616d35dbb55eec31f284cfc5df3c
Tommi Pöntinen <98650216+tommipontinen76@users.noreply.github.com>
2026-03-15 03:48:15 +0200

    Added link to Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,23 +1,27 @@
 import iconLemon from '../assets/lemon.svg'
 import iconPackage from '../assets/package.svg'
 import iconSettings from '../assets/settings.svg'
+import { NavLink } from "react-router"
 
 function Menu(props) {
 
     return (
         <div className="menu">
             <div>
-                <img src={iconLemon} alt="main" />
+                <NavLink to="/"><img src={iconLemon} alt="main" /></NavLink>
             </div>
             <div>
-                <img src={iconPackage} alt="store" />
-                { props.items ? <span className="menu_badge">{props.items}</span> : null }
+                <NavLink to="/store">
+                    <img src={iconPackage} alt="store" />
+                    { props.items ? <span className="menu_badge">{props.items}</span> : null }
+                </NavLink>
             </div>
             <div>
-                <img src={iconSettings} alt="settings" />
+                <NavLink to="/settings"><img src={iconSettings} alt="settings" /></NavLink>
             </div>
         </div>
     )
+
 
 }
 
