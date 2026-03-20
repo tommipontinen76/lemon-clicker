1280afbf8d6434a3afcda94f5d7439839ad12c1c
Tommi Pöntinen <98650216+tommipontinen76@users.noreply.github.com>
2026-03-20 12:54:14 +0200

    Lisätty tuotelistataulukko

---
+++ src/config/items.js
@@ -0,0 +1,128 @@
+export default [
+    {
+        id: "lemontree",
+        name: "Lemon tree",
+        qty: 0,
+        baseprice: 10,
+        price: 10,
+        multiplier: 0.2,
+        image: "lemontree.svg"
+    },
+    {
+        id: "blender",
+        name: "Blender",
+        qty: 0,
+        baseprice: 70,
+        price: 70,
+        multiplier: 2,
+        image: "blender.svg"
+    },
+    {
+        id: "carbonator",
+        name: "Carbonator",
+        qty: 0,
+        baseprice: 490,
+        price: 490,
+        multiplier: 10,
+        image: "carbonator.svg"
+    },
+    {
+        id: "bottler",
+        name: "Bottler",
+        qty: 0,
+        baseprice: 3400,
+        price: 3400,
+        multiplier: 50,
+        image: "bottler.svg"
+    },
+    {
+        id: "truck",
+        name: "Truck",
+        qty: 0,
+        baseprice: 24000,
+        price: 24000,
+        multiplier: 235,
+        image: "truck.svg"
+    },
+    {
+        id: "spring",
+        name: "Spring",
+        qty: 0,
+        baseprice: 169000,
+        price: 169000,
+        multiplier: 1150,
+        image: "spring.svg"
+    },
+    {
+        id: "sugarcane",
+        name: "Sugar cane field",
+        qty: 0,
+        baseprice: 1200000,
+        price: 1200000,
+        multiplier: 5600,
+        image: "sugarcane.svg"
+    },
+    {
+        id: "warehouse",
+        name: "Warehouse",
+        qty: 0,
+        baseprice: 8400000,
+        price: 8400000,
+        multiplier: 27500,
+        image: "warehouse.svg"
+    },
+    {
+        id: "lab",
+        name: "Lab",
+        qty: 0,
+        baseprice: 59000000,
+        price: 59000000,
+        multiplier: 135000,
+        image: "lab.svg"
+    },
+    {
+        id: "secret",
+        name: "Secret ingredient",
+        qty: 0,
+        baseprice: 417000000,
+        price: 417000000,
+        multiplier: 650000,
+        image: "secret.svg"
+    },
+    {
+        id: "power",
+        name: "Power plant",
+        qty: 0,
+        baseprice: 2950000000,
+        price: 2950000000,
+        multiplier: 3200000,
+        image: "power.svg"
+    },
+    {
+        id: "park",
+        name: "Amusement park",
+        qty: 0,
+        baseprice: 20800000000,
+        price: 20800000000,
+        multiplier: 15600000,
+        image: "park.svg"
+    },
+    {
+        id: "lemonai",
+        name: "LemonAI",
+        qty: 0,
+        baseprice: 147000000000,
+        price: 147000000000,
+        multiplier: 76100000,
+        image: "lemonai.svg"
+    },
+    {
+        id: "accelerator",
+        name: "Particle accelerator",
+        qty: 0,
+        baseprice: 1040000000000,
+        price: 1040000000000,
+        multiplier: 371000000,
+        image: "accelerator.svg"
+    },
+];
